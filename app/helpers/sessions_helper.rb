e5ed1ca25aadfcb22b455994505f35fe9a1fb224
Noor Ahmed Biswas <noor.ahmed.biswas@framgia.com>
2015-10-20 12:09:56 +0000

    upto 20/oct

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,18 @@ module	SessionsHelper
 				end
 		end
 		
+		def	create
+				user	=	User.find_by(email:	params[:session][:email].downcase)
+				if	user	&&	user.authenticate(params[:session][:password])
+						log_in	user
+						params[:session][:remember_me]	==	'1'	?	remember(user)	:	forget(user)
+						redirect_back_or	user
+				else
+						flash.now[:danger]	=	'Invalid	email/password	combination'
+						render	'new'
+				end
+		end
+		
 		def	forget(user)
 				user.forget
 				cookies.delete(:user_id)
@@ -34,10 +46,24 @@ module	SessionsHelper
 				cookies.permanent[:remember_token]	=	user.remember_token
 		end
 		
+		def	current_user?(user)
+				user	==	current_user
+		end
+		
 		def	log_out
 				forget(current_user)
 				session.delete(:user_id)
 				@current_user	=	nil
+		end
+		
+			#	Redirects	to	stored	location	(or	to	the	default).
+		def	redirect_back_or(default)
+				redirect_to(session[:forwarding_url]	||	default)
+				session.delete(:forwarding_url)
+		end
+		#	Stores	the	URL	trying	to	be	accessed.
+		def	store_location
+				session[:forwarding_url]	=	request.url	if	request.get?
 		end
 		
 end
